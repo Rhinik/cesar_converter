55b81f582da53e4b7a12fdd0be6e9a810330a24f
Rhinik <deknowny@gmail.com>
2020-08-12 01:01:08 +0300

    Add MIT license

---
+++ cesar_converter/__init__.py
@@ -0,0 +1,24 @@
+"""
+Provide tools for working with cesar cipher.
+
+More: [GitHub repository](https://github.com/Rhinik/cesar_convertor)
+"""
+
+from .alphabets import ALPHABETS
+from .alphabets import generate_alphabet
+from .alphabets import add_alphabet
+
+from .convert import decode
+from .convert import encode
+
+
+__all__ = [
+    "ALPHABETS",
+    "generate_alphabet",
+    "add_alphabet",
+    "decode",
+    "encode"
+]
+
+
+__version__ = "0.1.0"
